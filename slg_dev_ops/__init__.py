10634295808f9cb103bef377e5d3aebe0b146e1d
slgotting <steven.gotting@pinnacledesignlab.com>
2021-04-13 19:35:51 -0400

    initial

--- slg_dev_ops/__init__.py
+++ slg_dev_ops/__init__.py
@@ -2,11 +2,11 @@
 This module is callable via the command line
 but importing it does nothing
 """
+from install_nginx import install_nginx
 
-from colored import fg, bg, attr
 
 def main():
-    print(f"{fg('dark_orange')}{attr('bold')}You called me!{attr('reset')}")
+    pass
 
 
 if __name__ == "__main__":
